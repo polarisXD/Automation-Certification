d7a78d3bde09a02504f04862b335ff0269a1c349
David Adams <david.adams@sling.com>
2019-11-19 10:18:23 -0700

    Added challenge 3

---
+++ challenge3/challenge3.py
@@ -0,0 +1,30 @@
+import unittest
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+
+class Challenge1(unittest.TestCase):
+
+    def setUp(self):
+        # code to startup webdriver
+        self.driver = webdriver.Chrome("../chromedriver")
+
+    def tearDown(self):
+        # code to close webdriver
+        self.driver.close()
+
+    def test_challenge1(self):
+        # code for our test steps
+        self.driver.get("https://copart.com")
+        self.assertIn("Copart", self.driver.title)
+        popular_items = self.driver.find_elements_by_xpath('//*[@id="tabTrending"]/div[1]/div[2]/div/ul/li/a')
+        assert popular_items
+        assert len(popular_items) > 0
+        for item in popular_items:
+            item_link = item.get_attribute("href")
+            assert item_link
+            print(item.text + " - " + item_link)
+
+
+if __name__ == '__main__':
+    unittest.main();
