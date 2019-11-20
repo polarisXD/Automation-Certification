4883883237ed745c7fe933da5845ace342b5dd0e
David Adams <david.adams@sling.com>
2019-11-19 17:35:47 -0700

    Added challenge 4

---
+++ challenge4/challenge4.py
@@ -0,0 +1,22 @@
+import unittest
+from selenium import webdriver
+from challenge4.fibonacciSpelling import spellFibonacciNumber
+
+class Challenge1(unittest.TestCase):
+
+    def setUp(self):
+        # code to startup webdriver
+        self.driver = webdriver.Chrome("../chromedriver")
+        spellFibonacciNumber(22)
+
+    def tearDown(self):
+        # code to close webdriver
+        self.driver.close()
+
+    def test_challenge1(self):
+        # code for our test steps
+        print()
+
+
+if __name__ == '__main__':
+    unittest.main();
