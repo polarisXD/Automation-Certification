c518eba233b1411e0a8ce50c60ded611d2a80cca
David Adams <david.adams@sling.com>
2019-11-21 16:47:27 -0700

    Simplified challenge 2

--- challenge2/challenge2.py
+++ challenge2/challenge2.py
@@ -25,22 +25,9 @@ class Challenge2(unittest.TestCase):
         search.send_keys(Keys.RETURN)
         assert "No results found" not in self.driver.page_source
         timeout = 10
-        WebDriverWait(self.driver, timeout).until(EC.presence_of_element_located((By.ID, "serverSideDataTable")))
-        table = self.driver.find_element_by_id("serverSideDataTable")
-        self.assertIsNotNone(table, "Unable to load table data")
-        table_rows = table.find_elements_by_tag_name("tr")
-        while len(table_rows) <= 1:
-            table_rows = table.find_elements_by_tag_name("tr")
-        porsche_present = False
-        checked = 0
-        for row in table_rows:
-            checked += 1
-            if "PORSCHE" in row.text:
-                porsche_present = True
-                break
-        print("Items checked: " + str(checked))
-        print("Total items: " + str(len(table_rows)))
-        self.assertTrue(porsche_present, "Could not locate PORSCHE in results.")
+        wait = WebDriverWait(self.driver, timeout)
+        table = wait.until(EC.presence_of_element_located((By.ID, "serverSideDataTable"))).get_attribute("innerHTML")
+        assert "PORSCHE" in table
 
 
 if __name__ == '__main__':
