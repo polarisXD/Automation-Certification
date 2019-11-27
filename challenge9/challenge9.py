d72ce2c335c36851a9b213533604ea82b1db8cf7
David Adams <david.adams@sling.com>
2019-11-27 10:00:37 -0700

    Added challenge 9

---
+++ challenge9/challenge9.py
@@ -0,0 +1,34 @@
+import unittest
+from selenium import webdriver
+from copartAPI import CopartAPI
+
+class Challenge9(unittest.TestCase):
+
+    def setUp(self):
+        # code to startup webdriver
+        self.driver = webdriver.Chrome("../chromedriver")
+        self.api = CopartAPI()
+
+    def tearDown(self):
+        # code to close webdriver
+        self.driver.close()
+
+    def test_challenge9(self):
+        # code for our test steps
+        try:
+            data = self.api.search("toyota camry")
+        except ConnectionError:
+            print("Response error")
+            assert False
+        content_element = data["data"]["results"]["content"][0]
+        assert content_element
+        assert type(content_element) is dict
+        assert type(content_element["lotNumberStr"]) is str
+        assert type(content_element["ln"]) is int
+        assert type(content_element["mkn"]) is str
+        for key in content_element.keys():
+            print(key + ":" + str(content_element[key]) + " data type: " + str(type(content_element[key])))
+
+
+if __name__ == '__main__':
+    unittest.main();
